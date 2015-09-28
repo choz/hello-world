5f3bba3c82e645ecc04608697d3e12b551c27884
choz <choz@WS-GUEST.key-software.com>
2015-09-28 11:18:20 +0200

    version 1.1.1 dev

--- src/test/Test.java
+++ src/test/Test.java
@@ -16,6 +16,6 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("version 1.1");    }
+        System.out.println("version 1.1.1");    }
     
 }
